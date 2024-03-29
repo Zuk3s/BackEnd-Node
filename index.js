409af9101751cb4d76d4cb8b595fc1a9e48b0b2f
Zuk3s <felipemarcedo_@hotmail.com>
2024-03-28 23:06:04 -0300

    create connection and routes

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import movieRoute from "./src/routes/movies.js";
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+//ROUTES
+app.use("/movies", movieRoute);
+
+// Connect to MongoDB database
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`${error} could not connect to the database`);
+  });
